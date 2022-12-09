43e67bb5764e1809d3170f953d3a593b34282f4e
Eduardo Catarino <eduardo.catarino@bee-eng.pt>
2022-12-09 17:01:05 +0000

    redone tests

--- src/src-default/TheToken.sol
+++ src/src-default/TheToken.sol
@@ -6,8 +6,8 @@ import "@layerzerolabs/token/oft/OFT.sol";
 
 /// @title TheToken
 /// @notice This contract is of the reward token for the vault, it works with omnichain
-contract TheToken is OFT {
-    constructor(address _layerZeroEndpoint) OFT("TheToken", "TT", _layerZeroEndpoint) {
+contract TheToken is OFT{
+    constructor(address _layerZeroEndpoint) OFT("TheToken", "TT", _layerZeroEndpoint){
         _mint(_msgSender(), type(uint256).max);
     }
 }
--- test/Tests.t.sol
+++ test/Tests.t.sol
@@ -20,17 +20,14 @@ contract Tests is UniswapTest {
     address public depositer1 = vm.addr(1502);
     address public depositer2 = vm.addr(1503);
     address public depositer3 = vm.addr(1504);
+    address public depositer4 = vm.addr(1505);
 
     //Goerli (Ethereum Testnet)​
     address public layerZeroEndpoint =  0xbfD2135BFfbb0B5378b56643c2Df8a87552Bfa23;
     
-    /// @notice This function inherits the setUp form UniswapTest
+    /// @notice This function inherits the setUp form UniswapTest and sets up Vault usage
     function setUp() public override {
         super.setUp();
-    }
-
-    /// @notice In this function you can run several example tests (only one example can run at a time)
-    function test() public {
 
         vm.startPrank(deployer);
         //initialize token
@@ -48,25 +45,138 @@ contract Tests is UniswapTest {
         //Give necessary tokens to vault and depositers
         theToken.transfer(address(proxy), 10000 ether);
 
-        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer1,1000 ether));require(success, "failed");
-        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer2,1000 ether));require(success, "failed");
-        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer3,1000 ether));require(success, "failed");
+        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer1,100 ether));require(success, "failed");
+        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer2,100 ether));require(success, "failed");
+        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer3,100 ether));require(success, "failed");
+        (success,) =LPToken.call(abi.encodeWithSignature("transfer(address,uint256)", depositer4,100 ether));require(success, "failed");
 
         vm.stopPrank();
-        
-        // example1 tests a simple case with two depositers where one gets rewards halfway and at the end
-        example1();
-        // example2 tests a case with two depositers with diferent locking periods
-        //example2();
-        // example3 tests a case with 3 depositers that get rewards at diferent times and withdraw before getting rewards
-        //example3();
-        // example4 tests a case with 3 depositers with diferent locking periods who get rewards at diferent times
-        //example4();
     }
 
-    /// @notice tests a simple case with two depositers where one gets rewards halfway and at the end
-    function example1() public {
+    /// @notice tests the function deposit(address,uint256)
+    function testDeposit() public {
 
+        //set Tokens per duration
+        vm.startPrank(deployer);
+        uint duration = (1/2)*(360*24*60*60); //6 months in seconds
+        (bool success,) = address(proxy).call(abi.encodeWithSignature("setTokensPerDuration(uint256,uint256)",50 ether,duration));
+        require(success, "Set Tokens per Duration failed");
+        vm.stopPrank();
+
+         //depositor 1 deposits 5 ether for 6 months, then 10 ether 1 year
+        vm.startPrank(depositer1);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,1)); require(success, "Deposit failed");
+
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 10 ether));require(success,"failed");
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,2)); require(success, "Deposit failed");
+        vm.stopPrank();
+
+        //depositor 2 deposits 10 ether for 1 year
+        vm.startPrank(depositer2);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 10 ether));require(success,"failed");
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",10 ether,2)); require(success, "Deposit failed");
+        vm.stopPrank();
+
+        //depositor 3 deposits 20 ether for 2 year
+        vm.startPrank(depositer3);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 20 ether));require(success,"failed");
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",20 ether,4)); require(success, "Deposit failed");
+        vm.stopPrank();
+
+        //depositor 4 deposits 5 ether for 3 year, then 0 ether for 1 year (both will revert)
+        vm.startPrank(depositer4);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        vm.expectRevert();
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,6)); require(success, "Deposit failed");
+        
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        vm.expectRevert();
+        (success, ) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",0 ether,2)); require(success, "Deposit failed");
+        vm.stopPrank();
+    }
+
+    /// @notice tests the function withdraw(uint256)
+    function testWithdraw() public {
+        //set Tokens per duration
+        vm.startPrank(deployer);
+        uint duration = (1/2)*(360*24*60*60); //6 months in seconds
+        (bool success,) = address(proxy).call(abi.encodeWithSignature("setTokensPerDuration(uint256,uint256)",50 ether,duration));
+        require(success, "Set Tokens per Duration failed");
+        vm.stopPrank();
+
+        //depositor 1 deposits 5 ether for 6 months and tries to withdraw (will revert)
+        vm.startPrank(depositer1);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        (bool success1, bytes memory data1) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,1)); require(success1, "Deposit failed");
+        uint id1 = uint(bytes32(data1));
+
+        vm.expectRevert();
+        (success,) = address(proxy).call(abi.encodeWithSignature("withdraw(uint256)",id1)); require(success, "withdraw failed");
+        vm.stopPrank();
+
+        //depositor 2 deposits 5 ether for 6 months, waits 6 months then withdraws
+        vm.startPrank(depositer2);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        (bool success2, bytes memory data2) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,1)); require(success2, "Deposit failed");
+        uint id2 = uint(bytes32(data2));
+
+        vm.warp(block.timestamp + 1/2 * (360*24*60*60)+1);
+        (success,) = address(proxy).call(abi.encodeWithSignature("withdraw(uint256)",id2)); require(success, "withdraw failed");
+        vm.stopPrank();
+
+        //depositer 1 waits a year since depositing and withdraws
+        vm.startPrank(depositer1);
+        vm.warp(block.timestamp + 1/2 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("withdraw(uint256)",id1)); require(success, "withdraw failed");
+        vm.stopPrank();
+    }
+
+    /// @notice tests the function getRewards(uint256)
+    function testGetRewards() public {
+        //set Tokens per duration
+        vm.startPrank(deployer);
+        uint duration = (1/2)*(360*24*60*60); //6 months in seconds
+        (bool success,) = address(proxy).call(abi.encodeWithSignature("setTokensPerDuration(uint256,uint256)",50 ether,duration));
+        require(success, "Set Tokens per Duration failed");
+        vm.stopPrank();
+
+        //depositor 1 deposits 5 ether for 2 years, and retrieves rewards several times during locked period, tries again after 1 year
+        vm.startPrank(depositer1);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        (bool success1, bytes memory data1) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,4)); require(success1, "Deposit failed");
+        uint id1 = uint(bytes32(data1));
+
+        vm.warp(block.timestamp + 1/2 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("getRewards(uint256)",id1)); require(success, "getRewards failed");
+
+        vm.warp(block.timestamp + 1/2 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("getRewards(uint256)",id1)); require(success, "getRewards failed");
+
+        vm.warp(block.timestamp + 1 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("getRewards(uint256)",id1)); require(success, "getRewards failed");
+        assertEq((theToken.balanceOf(depositer1))/ 1e18, 200 ether/ 1e18);
+
+        vm.warp(block.timestamp + 1 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("getRewards(uint256)",id1)); require(success, "getRewards failed");
+        assertEq((theToken.balanceOf(depositer1))/ 1e18, 200 ether/ 1e18);
+        vm.stopPrank();
+
+        //depositor 2 deposits 5 ether for 6 months, and retrieves rewards after 1 year
+        vm.startPrank(depositer2);
+        (success,) = LPToken.call(abi.encodeWithSignature("approve(address,uint256)", address(proxy), 5 ether));require(success,"failed");
+        (bool success2, bytes memory data2) = address(proxy).call(abi.encodeWithSignature("deposit(uint256,uint256)",5 ether,1)); require(success2, "Deposit failed");
+        uint id2 = uint(bytes32(data2));
+
+        vm.warp(block.timestamp + 1 * (360*24*60*60));
+        (success,) = address(proxy).call(abi.encodeWithSignature("getRewards(uint256)",id2)); require(success, "getRewards failed");
+        assertEq((theToken.balanceOf(depositer2))/ 1e18, 50 ether/ 1e18);
+        vm.stopPrank();
+
+    }
+/*
+    /// @notice tests a simple case with two depositers where one gets rewards halfway and at the end
+    function testExample1() public {
         //set Tokens per duration
         vm.startPrank(deployer);
         uint duration = (1/2)*(360*24*60*60); //6 months in seconds
@@ -109,8 +219,7 @@ contract Tests is UniswapTest {
     }
 
     /// @notice tests a case with two depositers with diferent locking period
-    function example2() public {
-
+    function testExample2() public {
         //set Tokens per duration
         vm.startPrank(deployer);
         uint duration = (1)*(360*24*60*60); //1 year in seconds
@@ -152,8 +261,7 @@ contract Tests is UniswapTest {
     }
 
     /// @notice tests a case with 3 depositers that get rewards at diferent times and withdraw before getting rewards
-    function example3() public {
-
+    function testExample3() public {
         //set Tokens per duration
         vm.startPrank(deployer);
         uint duration = (1)*(360*24*60*60); //1 year in seconds
@@ -209,7 +317,7 @@ contract Tests is UniswapTest {
     }
 
     /// @notice tests a case with 3 depositers with diferent locking periods who get rewards at diferent times
-    function example4() public {
+    function testExample4() public {
 
         //set Tokens per duration
         vm.startPrank(deployer);
@@ -262,5 +370,5 @@ contract Tests is UniswapTest {
         assertEq((theToken.balanceOf(depositer3))/ 1e18, 525 ether/ 1e18);
         vm.stopPrank();
     }
-    
+*/
 }
